2642a0a89f6ecb73c38efdb7fe6b31c4959e6e7f
Brigham Byerly <6891883+byerlyb20@users.noreply.github.com>
2024-01-25 14:20:03 -0700

    Android ActivityIndicator follows theme foreground color

--- library/src/androidMain/kotlin/com/lightningkite/rock/views/direct/ActivityIndicator.android.kt
+++ library/src/androidMain/kotlin/com/lightningkite/rock/views/direct/ActivityIndicator.android.kt
@@ -1,6 +1,8 @@
 package com.lightningkite.rock.views.direct
 
+import android.content.res.ColorStateList
 import android.widget.ProgressBar
+import com.lightningkite.rock.models.Theme
 import com.lightningkite.rock.views.ViewDsl
 import com.lightningkite.rock.views.ViewWriter
 
@@ -9,7 +11,10 @@ actual typealias NActivityIndicator = ProgressBar
 @ViewDsl
 actual fun ViewWriter.activityIndicator(setup: ActivityIndicator.() -> Unit) {
     return viewElement(factory = { ProgressBar(it, null, android.R.attr.progressBarStyleSmall) }, wrapper = ::ActivityIndicator) {
-        handleTheme(native)
+        handleTheme(native, foreground = {
+            theme: Theme, progressBar: ProgressBar ->
+            progressBar.indeterminateTintList = ColorStateList.valueOf(theme.foreground.colorInt())
+        })
         setup(this)
     }
 }
